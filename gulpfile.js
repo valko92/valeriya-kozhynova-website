c1af88c26326887aa7016e2fa08896dd88b90cfc
Valeriya K <valko92@hotmail.com>
2017-10-08 03:16:25 -0400

    first commit with super placehoder-y content.

---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+var path =  require('path');
+var fs =  require('fs');
+var del =  require('del');
+var gulp =  require('gulp');
+var gutil = require('gulp-util');
+var data =  require('gulp-data');
+var rename = require('gulp-rename');
+var minifyInline = require('gulp-minify-inline');
+var imagemin = require('gulp-imagemin');
+var pug =  require('gulp-pug');
+var sass =  require('gulp-sass');
+var prefix =  require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify =  require('gulp-uglify');
+var browserSync =  require('browser-sync');
+
+const PATHS = {
+  src:       './src/',
+  build:     './build/',
+  jsassets:  './build/assets/js',
+  cssassets: './build/assets/css',
+  scss:      './src/styles/',
+  js:        './src/scripts/',
+  public:    './src/public',
+  data:      './src/data/'
+};
+
+/** PUG -> HTML */
+var html = () => {
+  return gulp.src(`${PATHS.src}**/*.pug`)
+    .pipe(data((file) => {
+      if (fs.existsSync(`${PATHS.data}${path.basename(file.path)}.json`)) {
+        return JSON.parse(fs.readFileSync(`${PATHS.data}${path.basename(file.path)}.json`));
+      }
+    }))
+    .pipe(pug())
+    .on('error', (err) => {
+      process.stderr.write(`${err.message}\n`);
+      this.emit('end');
+    })
+    .pipe(rename(function (file) {
+      if (`${file.basename}${file.extname}` === 'index.html') {
+        gutil.noop();
+      } else if (`${file.basename}${file.extname}` === 'sitemap.html') {
+        file.dirname = '.';
+        file.extname = '.xml';
+      } else {
+        file.dirname = file.basename;
+        file.basename = 'index';
+      }
+    }))
+    .pipe(minifyInline())
+    .pipe(gulp.dest(PATHS.build));
+};
+
+/** SCSS -> CSS */
+var styles = () => {
+  return gulp.src(`${PATHS.scss}main.scss`)
+    .pipe(prefix({browsers: ['last 3 versions']}))
+    .pipe(gutil.env.env === 'dev' ? sass({outputStyle: 'expanded'}).on('error', sass.logError): sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest(`${PATHS.cssassets}`))
+    .pipe(gutil.env.env === 'dev' ? browserSync.stream() : gutil.noop());
+};
+
+/** JS -> TEST, UGLIFY */
+var scripts = () => {
+  return gulp.src(`${PATHS.js}*.js`)
+    .pipe(concat('scripts.js'))
+    .pipe(gutil.env.env === 'dev' ? uglify({output: { beautify: true }, mangle: false, compress: false}) : uglify({mangle: true}))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(`${PATHS.jsassets}`));
+};
+
+/** COPY + IMAGEMIN */
+var copy = () => {
+  return gulp.src(`${PATHS.public}/**/*`)
+    .pipe(gutil.env.env === 'dev' ? gutil.noop(): imagemin())
+    .pipe(gulp.dest(`${PATHS.build}`));
+};
+
+/** DELETE ./BUILD */
+var prep = () => {
+  return del([PATHS.build]);
+};
+
+/** WATCH + LOCAL SERVER */
+var reload = browserSync.reload;
+var localserver = () => {
+  browserSync.init({
+    server: PATHS.build,
+    port: 3000,
+    open: 'local',
+    cors: true,
+    reloadOnRestart: true
+  });
+  
+  // watch the src files and invoke tasks if they change
+  gulp.watch([`${PATHS.src}**/*.pug`, PATHS.data], html);
+  gulp.watch(PATHS.scss, styles);
+  gulp.watch(PATHS.js, scripts);
+  gulp.watch(PATHS.public, copy);
+
+  // watch the build files and reload the browser if they change because of the previous changes
+  gulp.watch([`${PATHS.build}**/*.html`,`${PATHS.data}*.json`, `${PATHS.build}**/*.js`,`${PATHS.build}public/**/*`]).on('change', () => {
+      reload();
+  });
+  gulp.watch(`${PATHS.build}**/*.css`).on('change', () => {
+    reload('main.min.css', {stream: true});
+  });
+};
+
+/** TASKS */
+gulp.task(html);
+html.description = 'Convert pug to html and xml while also using similarily-named json data files.';
+
+gulp.task(styles);
+styles.description = 'Convert scss to css, also include above the fold css.';
+
+gulp.task(scripts);
+scripts.description = 'Combine all javascript files into one and minify for production.';
+
+gulp.task(prep);
+prep.description = 'Delete the old build folder before you build/rebuild the project.';
+
+gulp.task(copy);
+copy.description = 'Copy everything in the .src/public/ folder directly into ./build/, compress any images on the way.';
+
+gulp.task(localserver);
+localserver.description = 'Spin up a local browser-sync server and reload when a change occurs.';
+
+gulp.task('default', gulp.series(prep, gulp.parallel(html, scripts, styles, copy)));
+
+gulp.task('serve', gulp.series(prep, gulp.parallel(html, scripts, styles, copy), localserver));
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Personal Resume Website
+
+This is my personal website to document my skillset, showoff my resume, and show off my web development prowess. It uses Javascript, SCSS styling, PUG html templates, YARN package manager, and GULP to tie it all together. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+* [Node.js](https://nodejs.org/en/) with npm package manager
+* (optional) [Yarn](https://yarnpkg.com/en/), if you don't want to use npm.
+
+
+### Installing
+
+First install all the packages with ``npm install`` or ``yarn``
+
+**build a prod-ready build folder** ``npm run build`` or ``yarn build``
+
+**dev build with local server** ``npm start`` or ``yarn start``
+
+**overview of all of the gulp tasks and what they do** ``npm run tasks`` or ``yarn tasks`` 
+
+
+## Running the tests
+
+No tests yet, will come at a later date. Static sites don't really require much testing.
+
+## Deployment
+
+Will deploy to an FTP server, either the usual way, or via Travis CI for testing, to be determined...
+
+## Built With
+
+* [PUG](https://pugjs.org/api/getting-started.html) - HTMl templates
+* [SCSS](http://sass-lang.com/documentation/file.SCSS_FOR_SASS_USERS.html) - Pre-processed css
+* [GULP](https://gulpjs.com/) - Task runner and builder
+* [NODE.JS](https://nodejs.org/en/) - Runs Gulp, tests and other scripts.
+
+
+# TODO:
+
+## website design ideas:
+
+* One page with scroll-to
+* Name really big at top - if multipage - change into each page title ?
+    * super quick cycle through all letters and land on one u need
+* super simple spaceous white text on color BG w alternating black text and white bg strips
+
